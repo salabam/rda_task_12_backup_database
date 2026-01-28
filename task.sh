d445cb781596895488081ec2de15d37b35f93b77
Pavlus Chaban <pavluschaban@MacBook-Pro-Olena.local>
2026-01-29 00:57:25 +0200

    Solution

--- task.sh
+++ task.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-mysqldump -u $DB_USER -p $DB_PASSWORD ShopDB | mysql -u $DB_USER -p $DB_PASSWORD ShopDBReserve;
+mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB | mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve;
 
-mysqldump -u $DB_USER -p $DB_PASSWORD ShopDB --no-create-info | mysql -u $DB_USER -p $DB_PASSWORD ShopDBDevelopment;
+mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB --no-create-info | mysql -u $DB_USER -p$DB_PASSWORD ShopDBDevelopment;
